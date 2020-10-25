6abbf5d92d5e3f5086eb6842376d363f747f3d05
Brian Praseuth <brianpraseuth@gmail.com>
2020-10-25 16:00:49 -0700

    Basic file system and connection working and set

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDouble with extra onions', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper no cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ultimate Jack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double Animal Style', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Patty no Sauce', false);
+
