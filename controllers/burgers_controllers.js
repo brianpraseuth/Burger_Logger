bada7da026749e5ca0490f476f40fe10714e9fc0
Brian Praseuth <brianpraseuth@gmail.com>
2020-10-27 11:23:21 -0700

    App is working but CSS won't connect

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -16,46 +16,34 @@ router.get("/", function(req, res) {
     });
   });
   
-//   router.post("/api/cats", function(req, res) {
-//     cat.create([
-//       "name", "sleepy"
-//     ], [
-//       req.body.name, req.body.sleepy
-//     ], function(result) {
-//       // Send back the ID of the new quote
-//       res.json({ id: result.insertId });
-//     });
-//   });
+  router.post("/api/burger", function(req, res) {
+    burger.insertOne([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
   
-//   router.put("/api/cats/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
+  router.put("/api/burger/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    var colVal = "devoured=" + req.body.devoured;
+    console.log("condition", condition);
   
-//     console.log("condition", condition);
+    burger.updateOne(
+      colVal
+    , condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
   
-//     cat.update({
-//       sleepy: req.body.sleepy
-//     }, condition, function(result) {
-//       if (result.changedRows == 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       } else {
-//         res.status(200).end();
-//       }
-//     });
-//   });
-  
-//   router.delete("/api/cats/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
-  
-//     cat.delete(condition, function(result) {
-//       if (result.affectedRows == 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       } else {
-//         res.status(200).end();
-//       }
-//     });
-//   });
   
   // Export routes for server.js to use.
   module.exports = router;
